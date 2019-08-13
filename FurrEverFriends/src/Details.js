f5b83fd82d25a94b19c8db06be6d720fcd3637d6
Andres Rafael Delgado <adelgado0723@gmail.com>
2019-08-13 15:48:29 -0400

    styled more details table

--- FurrEverFriends/src/Details.js
+++ FurrEverFriends/src/Details.js
@@ -3,33 +3,32 @@ import Carousel from './Carousel.js';
 import AnimalMap from './AnimalMap.js';
 import Modal from './Modal.js';
 
-let HtmlToReactParser = require('html-to-react').Parser;
-let htmlToReactParser = new HtmlToReactParser();
-
 class Details extends React.Component {
   state = { showModal: false };
 
   toggleModal = () => this.setState({ showModal: !this.state.showModal });
   render() {
-    const jsxDescription = htmlToReactParser.parse(
-      this.props.location.state.description
-    );
     const showModal = this.state.showModal;
 
     const optionalDetails = [];
-    for (detail in this.props.location.state.details) {
-      optionalDetails.push(
-        <div className="detail">
-          <span className="detail-header">
-            {detail
-              .toLowerCase()
-              .split(' ')
-              .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-              .join(' ')}
-          </span>
-          <span className="detail-value">{this.props.location.state.name}</span>
-        </div>
-      );
+    for (let detail in this.props.location.state.details) {
+      if (this.props.location.state.details[detail]) {
+        optionalDetails.push(
+          <div className="detail">
+            <span className="detail-header">
+              {detail
+                .toLowerCase()
+                .split(' ')
+                .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+                .join(' ')}
+              :
+            </span>
+            <span className="detail-value">
+              {this.props.location.state.name}
+            </span>
+          </div>
+        );
+      }
     }
 
     const details = (
@@ -59,20 +58,26 @@ class Details extends React.Component {
         {optionalDetails}
       </div>
     );
+
     return (
       <div className="details">
         <h1>{this.props.location.state.name}</h1>
         <Carousel media={this.props.location.state.media} />
         <AnimalMap location={this.props.location.state.location} />
-        <button onClick={this.toggleModal}>View Details</button>
-        <div className="description">{jsxDescription}</div>
+        <button onClick={this.toggleModal}>More Details</button>
+
+        <div
+          className="description"
+          dangerouslySetInnerHTML={{
+            __html: this.props.location.state.description,
+          }}
+        ></div>
         {showModal ? (
           <Modal>
             <h1>{this.props.location.state.name}'s Details</h1>
             {details}
             <div className="buttons">
               <button onClick={this.toggleModal}>Close</button>
-              {/* <button onClick={this.toggleModal}>No</button> */}
             </div>
           </Modal>
         ) : null}
